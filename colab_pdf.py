1bb5249c3183dbe9563e8300c696d9baf6ee5742
Anthony Ismael Manotoa Moreno <aimm1999@gmail.com>
2021-04-15 18:28:20 -0500

    more fixes

--- colab_pdf.py
+++ colab_pdf.py
@@ -8,7 +8,7 @@ def colab_pdf(file_name, notebookpath="/content/drive/MyDrive/Universidad/10 Cic
         )
 
     # Using the defaults used by google.colab
-    drive_mount_point = "/content/drive/MyDrive/Universidad/10 Ciclo/Python/"
+    drive_mount_point = "/content/drive/"
     gdrive_home = os.path.join(drive_mount_point, "MyDrive/Universidad/10 Ciclo/Python/")
 
     # If the drive is not already mounted, attempt to mount it.
